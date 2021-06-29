38bb6d9c1cc32838f276e1d6d023a782689bf3cf
phi <phi@freimann.eu>
2021-06-29 07:50:20 +0200

    Neuer verzeichnisname "aufgaben" statt Prüfungsaufgaben

--- layout/makeBoth.sh
+++ layout/makeBoth.sh
@@ -20,7 +20,7 @@ echo "Tex main Parent: ${TEX_MAIN_PARENT}"
 ##export BBW_KAPITEL_DIR=${TEX_MAIN_PARENT}/includes/kapitel
 ## neu ab Dez. 2020: nur noch die neuen Unterverzeichnisse in "thema" untersuchen.
 export BBW_THEMA_DIR=${TEX_MAIN_PARENT}/includes/thema
-export BBW_PRUEFUNGS_AUFGABEN_DIR=${TEX_MAIN_PARENT}/pruefungen/pruefungsAufgaben
+export BBW_PRUEFUNGS_AUFGABEN_DIR=${TEX_MAIN_PARENT}/pruefungen/aufgaben
 export BBW_REPETITIONS_AUFGABEN_DIR=${TEX_MAIN_PARENT}/includes/repetition
 
 declare -x TEXINPUTS="${MAKE_DIR}:${TEX_LAYOUT_DIR}:${BBW_PRUEFUNGS_AUFGABEN_DIR}:${BBW_REPETITIONS_AUFGABEN_DIR}:${BBW_KAPITEL_DIR}:${BBW_THEMA_DIR}:"
