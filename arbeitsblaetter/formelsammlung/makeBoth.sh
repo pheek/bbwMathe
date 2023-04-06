dd2f1f5fd2bca396c5886e6d856731974dbeeb03
phi <phi@freimann.eu>
2023-04-06 16:08:06 +0200

    Formelsammlung noch weiter makro-isiert

--- arbeitsblaetter/formelsammlung/makeBoth.sh
+++ arbeitsblaetter/formelsammlung/makeBoth.sh
@@ -1,10 +1,29 @@
 rm -f *.pdf
+
 cp VERSION_HECH.txt VERSION.sty
 . ./superMakeBoth.sh
 mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_HECH.pdf
-mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_HECH_TRAINER.pdf
-
+#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_HECH_TRAINER.pdf
 
 cp VERSION_RLP.txt VERSION.sty
 . ./superMakeBoth.sh
+mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_RLP.pdf
+#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_RLP_TRAINER.pdf
+
+cp VERSION_STEVE.txt VERSION.sty
+. ./superMakeBoth.sh
+mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_STEVE.pdf
+#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_STEVE_TRAINER.pdf
+
+cp VERSION_BECH.txt VERSION.sty
+. ./superMakeBoth.sh
+mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_BECH.pdf
+#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_BECH_TRAINER.pdf
+
+cp VERSION_FP.txt VERSION.sty
+. ./superMakeBoth.sh
+mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_FP.pdf
+#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_FP_TRAINER.pdf
+
+
 
