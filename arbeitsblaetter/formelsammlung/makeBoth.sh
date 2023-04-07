e5899f9203f0d49302a8f0840513677fc5a8b522
phi <phi@freimann.eu>
2023-04-07 10:24:09 +0200

    Versionen in der Formelsammlung noch einfacher konfigurierbar gemacht

--- arbeitsblaetter/formelsammlung/makeBoth.sh
+++ arbeitsblaetter/formelsammlung/makeBoth.sh
@@ -1,29 +1,31 @@
 rm -f *.pdf
 
-cp VERSION_HECH.txt VERSION.sty
-. ./superMakeBoth.sh
-mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_HECH.pdf
-#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_HECH_TRAINER.pdf
-
+# Version Rahmenlehrplan (RLP)
 cp VERSION_RLP.txt VERSION.sty
 . ./superMakeBoth.sh
 mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_RLP.pdf
-#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_RLP_TRAINER.pdf
 
-cp VERSION_STEVE.txt VERSION.sty
+
+# Hersberger Christian (HECH)
+cp VERSION_HECH.txt VERSION.sty
 . ./superMakeBoth.sh
-mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_STEVE.pdf
-#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_STEVE_TRAINER.pdf
+mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_HECH.pdf
 
+# Deubelbeiss Steve (DEST)
+cp VERSION_DEST.txt VERSION.sty
+. ./superMakeBoth.sh
+mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_DEST.pdf
+
+# Benz Christoph (BECH)
 cp VERSION_BECH.txt VERSION.sty
 . ./superMakeBoth.sh
 mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_BECH.pdf
-#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_BECH_TRAINER.pdf
 
-cp VERSION_FP.txt VERSION.sty
+# Freimann Gressly Philipp (GRPH)
+cp VERSION_GRPH.txt VERSION.sty
 . ./superMakeBoth.sh
-mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_FP.pdf
-#mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_FP_TRAINER.pdf
+mv FormelsammlungGESO_GESO.pdf FormelsammlungGESO_GRPH.pdf
+mv FormelsammlungGESO_TRAINER_GESO.pdf FormelsammlungGESO_TRAINER.pdf
 
 
 
