4af1a69a5fb57d15f8710fd6843dc4904760a89a
pheek <git@gress.ly>
2023-07-27 19:58:52 +0200

    Arbeitsblateter Vektorgeometirie Vektoren in 3D ergänzt

--- arbeitsblaetter/clean.sh
+++ arbeitsblaetter/clean.sh
@@ -13,3 +13,6 @@ fi
 if [ -f '../../../../../framework/bin/clean.sh' ] ; then
 . ../../../../../framework/bin/clean.sh
 fi
+if [ -f '../../../../../../framework/bin/clean.sh' ] ; then
+. ../../../../../../framework/bin/clean.sh
+fi
