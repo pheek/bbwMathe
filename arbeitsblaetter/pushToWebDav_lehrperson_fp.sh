23b7979446f5033990546385d41e81ec696a494f
pheek <git@gress.ly>
2023-08-03 20:32:16 +0200

    Neues Arbeitsblatt zum allgemeinen Funktionsbegriff

--- arbeitsblaetter/pushToWebDav_lehrperson_fp.sh
+++ arbeitsblaetter/pushToWebDav_lehrperson_fp.sh
@@ -56,8 +56,11 @@ cp -f ./funktionen/funktionen_leere_koordinatensysteme/LeereKoordinatensysteme.p
 echo "  copying Begriffe Koordinatensystem Selbsteinschätzung"
 cp -f ./funktionen/begriffe/Begriffe_ALLG.pdf ${BBW_WEBDAV_PATH}/lehrperson/fp/i_allg/thema/funktionen/einstieg/begriffe/Begriffe.pdf
 
-echo "  copying Koordinatensystem Arbeitsblatt SAN"
+echo "  copying Koordinatensystem Arbeitsblatt"
 cp -f ./funktionen/koordinatensystem_arbeitsblatt/*.pdf ${BBW_WEBDAV_PATH}/lehrperson/fp/i_allg/thema/funktionen/koordinatensystem/arbeitsblatt/
+
+echo "  copying Funktionsbegriff Arbeitsblatt"
+cp -f ./funktionen/funktionsbegriff_arbeitsblatt/*.pdf ${BBW_WEBDAV_PATH}/lehrperson/fp/i_allg/thema/funktionen/funktionsbegriff/
 
 echo "  copying Lineare Funktionen Arbeitsblatt SAN"
 cp -f ./funktionen/funktionen_lineare_arbeitsblatt/*.pdf ${BBW_WEBDAV_PATH}/lehrperson/fp/i_allg/thema/funktionen/lineare_funktionen/ArbeitsblattSAN/
