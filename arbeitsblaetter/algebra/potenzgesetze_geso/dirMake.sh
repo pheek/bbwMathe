57d5a0bff4273c04b9ec120dfbc3c08b0c76f071
phi <phi@freimann.eu>
2024-03-06 22:42:21 +0100

    Neue Version "Transitional"

--- arbeitsblaetter/algebra/potenzgesetze_geso/dirMake.sh
+++ arbeitsblaetter/algebra/potenzgesetze_geso/dirMake.sh
@@ -1,1 +1,4 @@
-/home/phi/mgit/cnt/arbeitsblaetter/dirMake.sh
+##declare -x TEXINPUTS="/home/phi/mgit/cnt/thema:/home/phi/mgit/framework:."
+export TEXINPUTS=".:/home/phi/mgit/framework:/home/phi/mgit/cnt/thema:"
+pdflatex Potenzgesetze_GESO_noTRAINER.tex
+pdflatex Potenzgesetze_GESO_TRAINER.tex
--- arbeitsblaetter/dirMake.sh
+++
@@ -1,4 +0,0 @@
-##declare -x TEXINPUTS="/home/phi/mgit/cnt/thema:/home/phi/mgit/framework:."
-export TEXINPUTS=".:/home/phi/mgit/framework:/home/phi/mgit/cnt/thema:"
-pdflatex Potenzgesetze_GESO_noTRAINER.tex
-pdflatex Potenzgesetze_GESO_TRAINER.tex
