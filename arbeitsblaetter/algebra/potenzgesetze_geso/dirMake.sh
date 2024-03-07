e6e8b28813e1a15a02365daf2cf40c4467a8aeba
phi <phi@freimann.eu>
2024-03-07 07:49:16 +0100

    Tranistion und Aufgabenbeschreibung

--- arbeitsblaetter/algebra/potenzgesetze_geso/dirMake.sh
+++ arbeitsblaetter/algebra/potenzgesetze_geso/dirMake.sh
@@ -1,4 +1,7 @@
-##declare -x TEXINPUTS="/home/phi/mgit/cnt/thema:/home/phi/mgit/framework:."
+#!/usr/bin/bash
+# Transition 2024
+# uses no "Zielgruppe" any more.
+
 export TEXINPUTS=".:/home/phi/mgit/framework:/home/phi/mgit/cnt/thema:"
 pdflatex Potenzgesetze_GESO_noTRAINER.tex
 pdflatex Potenzgesetze_GESO_TRAINER.tex
