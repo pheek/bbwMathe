cda0ba513ae06944d09feb4e3cff99a1d6ce49fc
phi <phi@freimann.eu>
2024-03-31 19:15:02 +0200

    Neue Beispiele zum dirMake.sh anstelle von makeBoth.sh

---
+++ arbeitsblaetter/algebra/potenzgesetze/dirMake.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# Transition 2024
+# uses no "Zielgruppe" any more.
+
+export TEXINPUTS=".:/home/phi/mgit/framework:/home/phi/mgit/cnt/thema:"
+pdflatex Potenzgesetze_ALLG.tex
+pdflatex Potenzgesetze_TRAINER_ALLG.tex
