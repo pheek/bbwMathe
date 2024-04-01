adf6de75a2133a968d651bc3a24114d1405874b9
phi <phi@freimann.eu>
2024-04-01 19:29:57 +0200

    Reihenfolge Namensgebung

--- skripts/TALS_S_VECG2/dirMake.bat
+++ skripts/TALS_S_VECG2/dirMake.bat
@@ -4,5 +4,5 @@
 
 #export TEXINPUTS=".:C:\users\....    C:\mgit\framework   ...   C:\mgit\cnt\thema"
 pdflatex TALS_S_VECG2_TALS.tex
-pdflatex TALS_S_VECG2_TALS_TRAINER.tex
+pdflatex TALS_S_VECG2_TRAINER_TALS.tex
 
