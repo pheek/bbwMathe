fd3f3eab8131c2a696712c8e3d89db6c8eb7cd24
phi <phi@freimann.eu>
2024-04-06 15:11:26 +0200

    Geso 1j umgebaut

---
+++ skripts/GESO_1J/dirMake.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Transition 2024
+# uses no "Zielgruppe" any more.
+
+export TEX_FILE_NAME=GESO_1J_GESO
+../dirMake.sh
+
+export TEX_FILE_NAME=GESO_1J_TRAINER_GESO
+../dirMake.sh
+
