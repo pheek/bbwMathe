87c6bfc3b1eb6281d5d8151a2258f65a708c0950
phi <phi@freimann.eu>
2024-04-06 15:26:27 +0200

    GESO Grundlagenfach in reine LaTeX-Versionen umgebaut

---
+++ skripts/GESO_BLENDED/dirMake.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Transition 2024
+# uses no "Zielgruppe" any more.
+
+export TEX_FILE_NAME=GESO_BLENDED_GESO
+../dirMake.sh
+
+export TEX_FILE_NAME=GESO_BLENDED_TRAINER_GESO
+../dirMake.sh
+
