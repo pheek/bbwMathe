257de7a59f36e7859b4d05e4cf0a501e6ce0a064
phi <phi@freimann.eu>
2024-04-12 15:55:23 +0200

    Arbeitsblätter weiter im Vereinfachen der Skripts

--- arbeitsblaetter/datenanalyse/DiagrammeLesen/dirMake.sh
+++ arbeitsblaetter/datenanalyse/DiagrammeLesen/dirMake.sh
@@ -1,8 +1,7 @@
-
 #!/usr/bin/bash
-# Transition 2024
-# uses no "Zielgruppe" any more.
 
-export TEXINPUTS=".:/home/phi/mgit/framework:/home/phi/mgit/cnt/thema:"
-pdflatex DiagrammeLesen_ALLG.tex
-pdflatex DiagrammeLesen_TRAINER_ALLG.tex
+export TEX_FILE_NAME=DiagrammeLesen_ALLG
+../../dirMake2.sh
+
+export TEX_FILE_NAME=DiagrammeLesen_TRAINER_ALLG
+../../dirMake2.sh
