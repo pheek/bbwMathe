d5aa901bb6ca655d0de64f71ccf910c286b6d8f8
phi <phi@freimann.eu>
2024-08-07 09:08:50 +0200

    Weitere Aufgaben Vektorgeometrie

--- arbeitsblaetter/geometrie/vektorgeometrie/vecg2/Geradengleichung/dirMake.sh
+++ arbeitsblaetter/geometrie/vektorgeometrie/vecg2/Geradengleichung/dirMake.sh
@@ -4,5 +4,7 @@
 
 export TEXINPUTS=".:/home/phi/mgit/framework:/home/phi/mgit/cnt/thema:"
 pdflatex Geradengleichung_TALS.tex
+pdflatex Geradengleichung_TALS.tex
+pdflatex Geradengleichung_TRAINER_TALS.tex
 pdflatex Geradengleichung_TRAINER_TALS.tex
 
