4e18fbd5d5833d12e4cae26f105bafde235a0c13
phi <phi@freimann.eu>
2024-10-05 21:23:31 +0200

    Neue Makros

--- arbeitsblaetter/algebra/bruchrechnen/Uebungen/dirMake.sh
+++ arbeitsblaetter/algebra/bruchrechnen/Uebungen/dirMake.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
-export TEX_FILE_NAME=BruchrechnenUebungen_ALLG
+export TEX_FILE_NAME=BruchrechnenUebungen_MitPlatz
 ../../../dirMake2.sh
 
-export TEX_FILE_NAME=BruchrechnenUebungen_TRAINER_ALLG
+export TEX_FILE_NAME=BruchrechnenUebungen_TRAINER
+../../../dirMake2.sh
+
+export TEX_FILE_NAME=BruchrechnenUebungen_NurAufgaben
 ../../../dirMake2.sh
