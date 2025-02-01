9f8154eaca4b7daf7ac7ff11c0cda4adcba22143
phi <phi@freimann.eu>
2025-02-01 18:55:27 +0100

    Kompilieren

--- skripts/TALS_4J_GLF_ALL/dirMake.sh
+++ skripts/TALS_4J_GLF_ALL/dirMake.sh
@@ -4,7 +4,9 @@
 
 export TEX_FILE_NAME=TALS_4J_GLF_ALL_TALS
 ../dirMake.sh
+../dirMake.sh
 
 export TEX_FILE_NAME=TALS_4J_GLF_ALL_TRAINER_TALS
+../dirMake.sh
 ../dirMake.sh
 
--- skripts/TALS_4J_SPF_ALL/dirMake.sh
+++ skripts/TALS_4J_SPF_ALL/dirMake.sh
@@ -4,7 +4,9 @@
 
 export TEX_FILE_NAME=TALS_4J_SPF_ALL_TALS
 ../dirMake.sh
+../dirMake.sh
 
 export TEX_FILE_NAME=TALS_4J_SPF_ALL_TRAINER_TALS
+../dirMake.sh
 ../dirMake.sh
 
