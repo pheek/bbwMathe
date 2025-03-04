1663f2c97ce566c0b9e52e66a0f3f811fbf90935
phi <phi@freimann.eu>
2025-03-04 15:12:53 +0100

    Neues Arbeitsblatt zu quadratischen Gleichungen begonnen

---
+++ arbeitsblaetter/gleichungen/quadratische/dirMake.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+export TEX_FILE_NAME=QuadratischeGleichungen_MitPlatz
+../../dirMake2.sh
+
+export TEX_FILE_NAME=QuadratischeGleichungen_TRAINER
+../../dirMake2.sh
+
+export TEX_FILE_NAME=QuadratischeGleichungen_NurAufgaben
+../../dirMake2.sh
