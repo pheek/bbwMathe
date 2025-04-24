64d23f17ed617cd4eaeda6133ca088ceda50cfb1
phi <phi@freimann.eu>
2025-04-24 18:10:17 +0200

    Kompendium fehlte ein Flag

--- arbeitsblaetter/GESO_Kompendium/versions/makeAll.sh
+++ arbeitsblaetter/GESO_Kompendium/versions/makeAll.sh
@@ -4,56 +4,56 @@
 
 cd ..
 
-clean.sh
+./clean.sh
 #killall evince
 #rm *.pdf
 
 #cp versions/kflagsDEU_Aufgaben_Raster.tex kflags.tex
 #lualatex kompendium.tex
 #makeindex kompendium.idx
-#lualatex kompendium.tex
+#pdflatex kompendium.tex
 #cp kompendium.pdf kompendium_Deutsch_Aufgaben_mit_Raster.pdf
 
 cp versions/kflagsDEU_Aufgaben_Kompakt.tex kflags.tex
-lualatex kompendium.tex
+pdflatex kompendium.tex
 makeindex kompendium.idx
-lualatex kompendium.tex
+pdflatex kompendium.tex
 cp kompendium.pdf kompendium_Deutsch_Aufgaben.pdf
 
 cp versions/kflagsDEU_Aufgaben_BMANiveau.tex kflags.tex
-lualatex kompendium.tex
+pdflatex kompendium.tex
 makeindex kompendium.idx
-lualatex kompendium.tex
+pdflatex kompendium.tex
 cp kompendium.pdf kompendium_Deutsch_BMP_Niveau.pdf
 
 cp versions/kflagsDEU_Loesungen.tex kflags.tex
-lualatex kompendium.tex
+pdflatex kompendium.tex
 makeindex kompendium.idx
-lualatex kompendium.tex
+pdflatex kompendium.tex
 cp kompendium.pdf kompendium_Deutsch_Loesungen.pdf
 
 #cp versions/kflagsENG_Aufgaben_Raster.tex kflags.tex
-#lualatex kompendium.tex
+#pdflatex kompendium.tex
 #makeindex kompendium.idx
-#lualatex kompendium.tex
+#pdflatex kompendium.tex
 #cp kompendium.pdf kompendium_English_Aufgaben_mit_Raster.pdf
 
-cp versions/kflagsENG_Aufgaben_Kompakt.tex kflags.tex
-lualatex kompendium.tex
-makeindex kompendium.idx
-lualatex kompendium.tex
-cp kompendium.pdf kompendium_English_Aufgaben.pdf
+#cp versions/kflagsENG_Aufgaben_Kompakt.tex kflags.tex
+#pdflatex kompendium.tex
+#makeindex kompendium.idx
+#pdflatex kompendium.tex
+#cp kompendium.pdf kompendium_English_Aufgaben.pdf
 
 cp versions/kflagsENG_Aufgaben_BMANiveau.tex kflags.tex
-lualatex kompendium.tex
+pdflatex kompendium.tex
 makeindex kompendium.idx
-lualatex kompendium.tex
+pdflatex kompendium.tex
 cp kompendium.pdf kompendium_English_BMP_Niveau.pdf
 
 cp versions/kflagsENG_Loesungen.tex kflags.tex
-lualatex kompendium.tex
+pdflatex kompendium.tex
 makeindex kompendium.idx
-lualatex kompendium.tex
+pdflatex kompendium.tex
 cp kompendium.pdf kompendium_English_Loesungen.pdf
 
 rm kompendium.pdf
