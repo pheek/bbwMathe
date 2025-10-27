e2c64229c96f0d3938e4b73aff33570546e0b4a2
phi <phi@freimann.eu>
2025-10-27 06:38:58 +0100

    Wieder das alte Skript, denn das läuft stabil.

--- skripts/dirMake.sh
+++ skripts/dirMake.sh
@@ -9,11 +9,14 @@ export TEXINPUTS=".:/home/phi/mgit/framework:/home/phi/mgit/cnt/thema:"
 # wirklich nötig sind. Leider Leider: läuft latexmk nicht auf meinem
 # alten Debian-Server.
 
-latexmk -pdf -silent -use-make ${TEX_FILE_NAME}.tex
+#latexmk -pdf -silent -use-make ${TEX_FILE_NAME}.tex
+# offensichtlich läuft zwar latexmk auf dem neuen Server, doch
+# es kommt zu einem "make" Problem, denn nicht alles war inkludiert wird,
+# wird mit "input" angezogen. Irgendwie muss ich da noch anpassen
 
-#pdflatex  ${TEX_FILE_NAME}.tex
-#bibtex    ${TEX_FILE_NAME}.aux
-#makeindex ${TEX_FILE_NAME}.idx
-#pdflatex  ${TEX_FILE_NAME}.tex
-#makeindex ${TEX_FILE_NAME}.idx
-#pdflatex  ${TEX_FILE_NAME}.tex
+pdflatex  ${TEX_FILE_NAME}.tex
+bibtex    ${TEX_FILE_NAME}.aux
+makeindex ${TEX_FILE_NAME}.idx
+pdflatex  ${TEX_FILE_NAME}.tex
+makeindex ${TEX_FILE_NAME}.idx
+pdflatex  ${TEX_FILE_NAME}.tex
